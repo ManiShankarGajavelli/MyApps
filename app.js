e7c0daed20bbf10f703cc412f73d4c279154f5fb
Gajavelli <manishankarg@cns-inc.com>
2019-11-29 10:52:49 +0530

    main files added.

---
+++ app.js
@@ -0,0 +1,10 @@
+import square from './app1.js';
+console.clear();
+console.log(square(20));
+
+ var a;
+ if(a==0){
+    console.log('a is positive'+ a);
+ }else{
+     console.log('a is negative'+ a);
+ }
