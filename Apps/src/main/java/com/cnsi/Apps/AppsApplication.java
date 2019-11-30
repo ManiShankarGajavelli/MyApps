aeb15df8110f1c2bf1ffbabcd60e465ada3ea0ac
manishankarg <manishankarg@CTI-LPT0415.cns-inc.com>
2019-12-01 00:27:03 +0530

    test

---
+++ Apps/src/main/java/com/cnsi/Apps/AppsApplication.java
@@ -0,0 +1,14 @@
+package com.cnsi.Apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsApplication {
+
+	public static void main(String[] args) {
+//		sysout
+		SpringApplication.run(AppsApplication.class, args);
+	}
+
+}
