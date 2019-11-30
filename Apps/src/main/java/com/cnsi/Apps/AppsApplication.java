09b65294a56e78817bf3b74006c83d4179ae51c0
manishankarg <manishankarg@CTI-LPT0415.cns-inc.com>
2019-12-01 00:10:18 +0530

    file changed

--- Apps/src/main/java/com/cnsi/Apps/AppsApplication.java
+++ Apps/src/main/java/com/cnsi/Apps/AppsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppsApplication {
 
 	public static void main(String[] args) {
+//		sysout
 		SpringApplication.run(AppsApplication.class, args);
 	}
 
